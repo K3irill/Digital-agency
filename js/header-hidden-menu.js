b1e632cd0374fe5914731ca4de75528fd8c5b096
K3rill <razdor.2019@mail.ru>
2024-08-22 11:38:47 +0300

    activating the hidden menu

---
+++ js/header-hidden-menu.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const headerElement = document.querySelector(".header");
+const headerHiddenMenu = document.querySelector(".header__hidden-menu");
+const headerBtnMenu = document.querySelector("#headMenuBtn");
+
+function makeActiveHHM() {
+  if (headerHiddenMenu.classList.contains("hidden-menu_active")) {
+    headerHiddenMenu.classList.remove("hidden-menu_active");
+    headerElement.classList.remove("header_bgc-white-js")
+  } else {
+    headerHiddenMenu.classList.add("hidden-menu_active");
+    headerElement.classList.add("header_bgc-white-js");
+  }
+}
+
+headerBtnMenu.addEventListener("click", function (e) {
+  makeActiveHHM(e);
+});
+
+
